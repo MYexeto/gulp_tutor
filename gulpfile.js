754676b09f6a6922a9dc496e57be7f25e79534ab
Myexeto <my.exeto@gmail.com>
2022-11-21 18:48:50 -0700

    added font and pictures to project

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ gulp.task('server', function() {
     });
 });
 
+
+
 gulp.task('styles', function() {
     return gulp.src("src/sass/*.+(scss|sass)")
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
