ed4f39a71d40c7c8970611f37872b3e9d3a7700b
Myexeto <my.exeto@gmail.com>
2022-11-28 12:40:44 -0700

    added items catalog with tabs.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-    $('.courusel__inner').slick();
+    $('.courusel__inner').slick({
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/chevron-right-solid.png"></button>',
+        responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+                infinite: true,
+                dots: true
+            }
+        }],
+    });
   });
