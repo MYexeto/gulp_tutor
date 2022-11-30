6b53fd190b52837d44834315c88aa096a2e2000a
Myexeto <my.exeto@gmail.com>
2022-11-30 14:06:00 -0700

    added modals and buttons reactions

--- src/js/script.js
+++ src/js/script.js
@@ -30,4 +30,25 @@ $(document).ready(function(){
       };
       toggleSLide('.catalog-item__back');
       toggleSLide('.catalog-item__link');
+
+      // Modal
+
+      $('[data-modal=consultation]').on('click', function(){
+        $('.overlay, #consultation').fadeIn();
+      });
+      $('.modal__close').on('click', function(){
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+      });
+
+
+      $('.button_mini').on('click', function() {
+        $('.overlay, #order').fadeIn('slow');
+      });
+
+
+      $('.button_mini').each(function(i){
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i))
+        });
+      });
   });
